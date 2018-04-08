27c7bed5eb8b731fed810a780c8da56527f76a61
Jakub Czachor (jakucza608) <jakcz96@gmail.com>
2018-04-08 02:16:04 +0200

    Commit 6 Designing GUI.

--- Registration Number Recognizer/GUI + CALCULATIONS/MainWindow.h
+++ Registration Number Recognizer/GUI + CALCULATIONS/MainWindow.h
@@ -19,7 +19,7 @@ namespace GUICALCULATIONS {
 	using namespace System::Runtime::InteropServices;
 	using namespace std;
 	using namespace cv;
-	Mat src; //need to create a Mat typpe for the src image.
+	Mat src; //need to create a Mat type for the src image.
 
 	/// <summary>
 	/// Summary for MainWindow
@@ -46,10 +46,13 @@ namespace GUICALCULATIONS {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^  btnBrowse;
-	private: System::Windows::Forms::Button^  btnProcess;
+
+
 	private: System::Windows::Forms::PictureBox^  ptbSource;
-	private: System::Windows::Forms::Label^  lbSource;
+	private: System::Windows::Forms::Button^  btnProcess;
+	private: System::Windows::Forms::Button^  btnLearn;
+	private: System::Windows::Forms::TextBox^  textBox1;
+
 
 	protected:
 
@@ -70,31 +73,12 @@ namespace GUICALCULATIONS {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->btnBrowse = (gcnew System::Windows::Forms::Button());
-			this->btnProcess = (gcnew System::Windows::Forms::Button());
 			this->ptbSource = (gcnew System::Windows::Forms::PictureBox());
-			this->lbSource = (gcnew System::Windows::Forms::Label());
+			this->btnProcess = (gcnew System::Windows::Forms::Button());
+			this->btnLearn = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ptbSource))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// btnBrowse
-			// 
-			this->btnBrowse->Location = System::Drawing::Point(12, 12);
-			this->btnBrowse->Name = L"btnBrowse";
-			this->btnBrowse->Size = System::Drawing::Size(90, 35);
-			this->btnBrowse->TabIndex = 0;
-			this->btnBrowse->Text = L"Browse";
-			this->btnBrowse->UseVisualStyleBackColor = true;
-			this->btnBrowse->Click += gcnew System::EventHandler(this, &MainWindow::btnBrowse_Click);
-			// 
-			// btnProcess
-			// 
-			this->btnProcess->Location = System::Drawing::Point(108, 12);
-			this->btnProcess->Name = L"btnProcess";
-			this->btnProcess->Size = System::Drawing::Size(93, 35);
-			this->btnProcess->TabIndex = 1;
-			this->btnProcess->Text = L"Process";
-			this->btnProcess->UseVisualStyleBackColor = true;
 			// 
 			// ptbSource
 			// 
@@ -105,24 +89,42 @@ namespace GUICALCULATIONS {
 			this->ptbSource->TabIndex = 2;
 			this->ptbSource->TabStop = false;
 			// 
-			// lbSource
+			// btnProcess
 			// 
-			this->lbSource->AutoSize = true;
-			this->lbSource->Location = System::Drawing::Point(356, 23);
-			this->lbSource->Name = L"lbSource";
-			this->lbSource->Size = System::Drawing::Size(73, 13);
-			this->lbSource->TabIndex = 3;
-			this->lbSource->Text = L"Source Image";
+			this->btnProcess->Location = System::Drawing::Point(12, 12);
+			this->btnProcess->Name = L"btnProcess";
+			this->btnProcess->Size = System::Drawing::Size(90, 35);
+			this->btnProcess->TabIndex = 5;
+			this->btnProcess->Text = L"Process";
+			this->btnProcess->UseVisualStyleBackColor = true;
+			this->btnProcess->Click += gcnew System::EventHandler(this, &MainWindow::btnProcess_Click);
+			// 
+			// btnLearn
+			// 
+			this->btnLearn->Location = System::Drawing::Point(108, 12);
+			this->btnLearn->Name = L"btnLearn";
+			this->btnLearn->Size = System::Drawing::Size(90, 35);
+			this->btnLearn->TabIndex = 6;
+			this->btnLearn->Text = L"Learn";
+			this->btnLearn->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(437, 20);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(128, 20);
+			this->textBox1->TabIndex = 7;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MainWindow::textBox1_TextChanged);
 			// 
 			// MainWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(577, 370);
-			this->Controls->Add(this->lbSource);
-			this->Controls->Add(this->ptbSource);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->btnLearn);
 			this->Controls->Add(this->btnProcess);
-			this->Controls->Add(this->btnBrowse);
+			this->Controls->Add(this->ptbSource);
 			this->Name = L"MainWindow";
 			this->Text = L"MainWindow";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ptbSource))->EndInit();
@@ -131,27 +133,29 @@ namespace GUICALCULATIONS {
 
 		}
 #pragma endregion
-	private: System::Void btnBrowse_Click(System::Object^  sender, System::EventArgs^  e) {
 
-		OpenFileDialog^ dgOpen = gcnew OpenFileDialog();
-		dgOpen->Filter = "Image(*.bmp; *.jpg)|*.bmp;*.jpg|All files (*.*)|*.*||";
-		if (dgOpen->ShowDialog() == System::Windows::Forms::DialogResult::Cancel)
-		{
-			return;
-		}
-		Bitmap^ bmpSrc = gcnew Bitmap(dgOpen->FileName);
-		ptbSource->Image = bmpSrc;
-		ptbSource->Refresh();
-		src = imread(ConvertString2Char(dgOpen->FileName));
-		imshow("Source image showing via OpenCV", src);
-
-	}
-
-	private: char* ConvertString2Char(System::String^ str) {
+	private: char* ConvertString2Char(System::String^ str) { //function converting  string to array of characters
 		char* str2 = (char*)(void*)Marshal::StringToHGlobalAnsi(str);
 		return str2;
 	}
 
 
+private: System::Void btnProcess_Click(System::Object^  sender, System::EventArgs^  e) { //Process button action
+																						 
+	//Open File Dialog (select a path)
+	OpenFileDialog^ dgOpen = gcnew OpenFileDialog();
+	dgOpen->Filter = "Image(*.bmp; *.jpg)|*.bmp;*.jpg|All files (*.*)|*.*||";
+	if (dgOpen->ShowDialog() == System::Windows::Forms::DialogResult::Cancel)
+	{
+		return;
+	}
+
+	//Showing image in picture box
+	Bitmap^ bmpSrc = gcnew Bitmap(dgOpen->FileName); //converting to bitmap
+	ptbSource->Image = bmpSrc;
+	ptbSource->Refresh();
+	src = imread(ConvertString2Char(dgOpen->FileName));
+	imshow("Source image showing via OpenCV", src); //new window
+}
 };
 }
